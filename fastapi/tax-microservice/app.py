124ba173ad9153252b51481ca2f3bad84da1e75b
BAT1CLJ2 <bat1clj2@bosch.com>
2023-06-06 15:44:36 +0300

    renamed and moved files and dirs

--- fastapi/tax-microservice/app.py
+++ fastapi/tax-microservice/app.py
@@ -1,19 +1,20 @@
 import asyncio
+import json
 import os
 from typing import List
 
 import motor.motor_asyncio
-from motor.motor_asyncio import AsyncIOMotorClient
 import uvicorn
-from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+from aiokafka import AIOKafkaConsumer
 from bson import ObjectId
 from dotenv import load_dotenv
-from fastapi import Body, FastAPI, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse, Response
+from fastapi.responses import JSONResponse
+from motor.motor_asyncio import AsyncIOMotorClient
+from regions import Region
 from pydantic import BaseModel, Field
-import json
-from poles import Pole
+
+from fastapi import Body, FastAPI, HTTPException, status
 
 load_dotenv()
 config = os.environ
@@ -43,7 +44,7 @@ class PyObjectId(ObjectId):
 
 class TaxModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    name: Pole = Field(...)
+    name: Region = Field(...)
     value: float = Field(...)
     quantity: int = Field(...)
 
@@ -90,34 +91,27 @@ async def listen_for_incoming_messages():
     global consumer
     consumer = AIOKafkaConsumer(
         bootstrap_servers=config.get('KAFKA_BOOTSTRAP_SERVERS'), loop=loop, value_deserializer=deserializer)
-    consumer.subscribe([config.get('TOPIC_PURCHASE_EVENT')])
+    consumer.subscribe(['purchase_events'])
     await consumer.start()
     try:
-        # data = await consumer.getmany(timeout_ms=10000)
-        # for _, messages in data.items():
-        #     for message in messages:
-        #         purchase = message.value
-        #         print(type(purchase), purchase)
-        #         await _calculate_new_tax(purchase)
         async for msg in consumer:
             print('Recieved kafka message:', msg)
             purchase = msg.value
-            print(type(purchase), purchase)
             await _calculate_new_tax(purchase)
     finally:
         await consumer.stop()
 
 
 async def _init_db():
-    for pole in list(Pole):
-        tax: TaxModel = TaxModel(name=pole, value=10, quantity=0)
+    for region in list(Region):
+        tax: TaxModel = TaxModel(name=region, value=10, quantity=0)
         tax = jsonable_encoder(tax)
         await db["taxes"].insert_one(tax)
 
 async def _calculate_new_tax(purchase: dict):
     taxes: list[TaxModel] = await list_taxes()
     taxes = jsonable_encoder(taxes)
-    target_tax: TaxModel = [tax for tax in taxes if tax['name'] == purchase['pole']][0]
+    target_tax: TaxModel = [tax for tax in taxes if tax['name'] == purchase['region']][0]
     target_tax = jsonable_encoder(target_tax)
     remaining_taxes = [tax for tax in taxes if tax != target_tax]
     new_tax_value = (target_tax['value'] + target_tax['quantity']) / (remaining_taxes[0]['value'] + remaining_taxes[1]['value'] + remaining_taxes[2]['value'] + (remaining_taxes[0]['quantity'] + remaining_taxes[1]['quantity'] + remaining_taxes[2]['quantity'])/3)
@@ -126,16 +120,12 @@ async def _calculate_new_tax(purchase: dict):
     updated_tax = await update_tax(target_tax['name'], UpdateTaxModel(value=target_tax['value'], quantity=target_tax['quantity']))
     print(updated_tax)
 
-@app.get('/hello')
-async def hello_world():
-    return {'result': 'Hello World!'}
-
-@app.get('/tax/{pole}')
-async def get_pole_tax(pole: str):
-    if (tax := await db["taxes"].find_one({"name": pole})) is not None:
+@app.get('/tax/{region}')
+async def get_region_tax(region: str):
+    if (tax := await db["taxes"].find_one({"name": region})) is not None:
         return tax['value']
 
-    raise HTTPException(status_code=404, detail=f"Tax with name {pole} is not found")
+    raise HTTPException(status_code=404, detail=f"Tax with name {region} is not found")
 
     
 @app.get(
@@ -145,21 +135,21 @@ async def list_taxes():
     taxes = await db["taxes"].find().to_list(1000)
     return taxes
 
-@app.put("/tax/{pole}", response_description="Update a tax", response_model=TaxModel)
-async def update_tax(pole: str, tax: UpdateTaxModel = Body(...)):
+@app.put("/tax/{region}", response_description="Update a tax", response_model=TaxModel)
+async def update_tax(region: str, tax: UpdateTaxModel = Body(...)):
     tax = {k: v for k, v in tax.dict().items() if v is not None}
-    update_result = await db["taxes"].update_one({"name": pole}, {"$set": tax})
+    update_result = await db["taxes"].update_one({"name": region}, {"$set": tax})
 
     if update_result.modified_count == 1:
         if (
-            updated_tax := await db["taxes"].find_one({"name": pole})
+            updated_tax := await db["taxes"].find_one({"name": region})
         ) is not None:
             return updated_tax
 
-    if (existing_tax := await db["taxes"].find_one({"name": pole})) is not None:
+    if (existing_tax := await db["taxes"].find_one({"name": region})) is not None:
         return existing_tax
 
-    raise HTTPException(status_code=404, detail=f"Tax with pole {pole} not found")
+    raise HTTPException(status_code=404, detail=f"Tax with region {region} not found")
 
 
 @app.delete("/", response_description="Delete all taxes")
